731cb83d01e09603cf576ff438ccca5d31116c9b
user <user@eax.ninja>
2025-06-15 13:43:18 +0200

    refactor(zsh): update environment vars

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -3,8 +3,7 @@
 
 export SHELL="/usr/bin/zsh"
 export EDITOR=nvim
-export BROWSER="/usr/bin/librewolf"
-# export BROWSER="/usr/bin/brave"
+export BROWSER="/usr/bin/brave"
 export SUDO_EDITOR="nvim"
 export DIFFPROG="nvim -d"
 export SYSTEMD_EDITOR=nvim
@@ -15,3 +14,5 @@ export MANROFFOPT="-c"
 export PATH="${PATH}:${HOME}/.cargo/bin"
 
 export STARSHIP_CONFIG="${HOME}/.config/zsh/starship.toml"
+
+export OGL_DEDICATED_HW_STATE_PER_CONTEXT="ENABLE_ROBUST"
