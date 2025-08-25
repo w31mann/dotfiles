7d600938a26fb9e852b4e6490ee7b99ae41c5710
user <user@eax.ninja>
2025-08-25 10:01:13 +0200

    refactor(zsh): add tmux alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,8 +5,7 @@ alias fd="fd --unrestricted"
 alias ip="ip -color=auto"
 alias rg="rg --smart-case --unrestricted"
 alias sudo="sudo "
+alias tm="tmux new-session -A -s main"
 alias v=nvim
 alias vim=nvim
 alias vimdiff="nvim -d"
-
-alias -s pdf="zathura"
