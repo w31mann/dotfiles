009d69d716f98a59771c8ba46bcd72d350c7eac2
user <user@eax.ninja>
2025-08-25 10:34:07 +0200

    feat(tmux): add tmux config

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,11 +5,7 @@ alias fd="fd --unrestricted"
 alias ip="ip -color=auto"
 alias rg="rg --smart-case --unrestricted"
 alias sudo="sudo "
+alias tm="tmux new-session -A -s main"
 alias v=nvim
 alias vim=nvim
 alias vimdiff="nvim -d"
-
-alias wrs2303="studio-cli --profile=wrs2303"
-alias wrs2309="studio-cli --profile=wrs2309"
-
-alias cargo-build-arm="cargo build --target aarch64-unknown-linux-musl --config target.aarch64-unknown-linux-musl.linker=\\\"aarch64-linux-gnu-gcc\\\""
