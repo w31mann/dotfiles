1062d8ad5764c2bd94c60d6e77306fa8a16374e9
user <user@eax.ninja>
2025-09-12 10:49:26 +0200

    refactor(tmux): update config

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,7 +5,6 @@ alias fd="fd --unrestricted"
 alias ip="ip -color=auto"
 alias rg="rg --smart-case --unrestricted"
 alias sudo="sudo "
-alias tm="tmux new-session -A -s main"
 alias v=nvim
 alias vim=nvim
 alias vimdiff="nvim -d"
