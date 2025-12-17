cd101fe62609e6d1f8733e33a7b0ab777fdb9643
user <user@eax.ninja>
2025-12-17 17:27:13 +0100

    refactor(waybar): update niri waybar config

--- waybar/.config/waybar/scripts/vpn-toggle.sh
+++ waybar/.config/waybar/scripts/vpn-toggle.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 if [[ $(networkctl | awk '$2 ~ "wg0" {print $4}') == *routable* ]]; then
-    kitty --app-id kitty-vpn --title "Disable VPN connection" \
-        zsh -c "echo 'Disable VPN connection' && sudo networkctl down wg0"
+    pkexec networkctl down wg0
 else
-    kitty --app-id kitty-vpn --title "Enable VPN connection" \
-        zsh -c "echo 'Enable VPN connection' && sudo networkctl up wg0"
-        sudo networkctl up wg0
+    pkexec networkctl up wg0
 fi
